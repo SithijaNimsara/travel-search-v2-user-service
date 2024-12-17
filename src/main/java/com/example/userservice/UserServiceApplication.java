38091e2396d92401e9d9e600f9facadf4d7546ce
Sithija <sithija9923@gamil.com>
2024-12-17 08:27:38 +0530

    changes in application.properties file

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,13 +1,19 @@
 package com.example.userservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);
+
     public static void main(String[] args) {
+        logger.info("UserServiceApplication --------- v2 -------- ");
         SpringApplication.run(UserServiceApplication.class, args);
+        logger.info("UserServiceApplication --------- v2 -------- ");
     }
 
 }
